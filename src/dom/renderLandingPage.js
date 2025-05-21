020a37da8663aa808cf4d85350d957c0fa4bea4d
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-21 23:33:40 +0530

    Change renderLandingPage code.

--- src/dom/renderLandingPage.js
+++ src/dom/renderLandingPage.js
@@ -6,27 +6,22 @@ export default function renderLandingPage() {
   const mainContainer = document.createElement("div");
   mainContainer.classList.add("body-container");
 
-  const welcomeHeading = document.createElement("h2");
-  welcomeHeading.innerText = "Welcome to weather";
+  mainContainer.innerHTML = `
+		<h2>Welcome to weather</h2>
+		<div class="location-div">
+			<h3>Enter a location</h3>
+			<form>
+				<input type="text" name="location" id="location" class="input-location">
+				<button type="submit" class="search-btn">Search</button>
+			</form>
+		</div>
+	`;
 
-  const enterMsg = document.createElement("h3");
-  enterMsg.innerText = "Enter a location";
-
-  const form = document.createElement("form");
-  form.innerHTML = `
-  <input type="text" name="location" id="location" class="input-location">
-  <button type="submit" class="search-btn">Search</button> 
-  `;
-
+  bodyEl.prepend(mainContainer);
+  const form = document.querySelector("form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const inputLocation = document.querySelector(".input-location");
     callAPIByLocation(inputLocation.value);
   });
-
-  mainContainer.appendChild(welcomeHeading);
-  mainContainer.appendChild(enterMsg);
-  mainContainer.appendChild(form);
-
-  bodyEl.prepend(mainContainer);
 }
