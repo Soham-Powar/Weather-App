ce84bc42e73b278347e33c095293b0b9bc3bf628
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-21 22:25:41 +0530

    Make a class to store day weather details.

--- src/modules/callAPIByLocation.js
+++ src/modules/callAPIByLocation.js
@@ -1,9 +1,15 @@
+import dayWeather from "./makeDayInfo";
+
 export default async function callAPIByLocation(locationName) {
-  const fetchURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?unitGroup=metric&key=24FXFJRPS3HUBDAAH3AFTTNJY&contentType=json`;
-  console.log(fetchURL);
+  const fetchURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}/next7days?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslikemax%2Cfeelslike%2Chumidity%2Cprecipprob%2Cvisibility%2Cuvindex%2Csunrise%2Cmoonphase%2Cconditions%2Cdescription%2Cicon&key=24FXFJRPS3HUBDAAH3AFTTNJY&contentType=json`;
+
   const response = await fetch(fetchURL, {
     mode: "cors",
   });
+
   const infoObject = await response.json();
   console.log(infoObject);
+
+  const day = new dayWeather(infoObject.days[0]);
+  console.log(day);
 }
---
+++ src/modules/makeDayInfo.js
@@ -0,0 +1,13 @@
+export default class dayWeather {
+  constructor(dayObject) {
+    this.temp = dayObject.temp;
+    this.tempMax = dayObject.tempmax;
+    this.tempMin = dayObject.tempmin;
+    this.humidity = dayObject.humidity;
+    this.rainProb = dayObject.precipprob;
+    this.uvIndex = dayObject.uvindex;
+    this.icon = dayObject.icon;
+    this.conditions = dayObject.conditions;
+    this.date = dayObject.datetime;
+  }
+}
