79d189b203fa6312949018d33c475d552ee28c94
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-21 22:44:30 +0530

    Rename handleRespObj function to extractData.

--- src/modules/callAPIByLocation.js
+++ src/modules/callAPIByLocation.js
@@ -1,5 +1,5 @@
 import dayWeather from "./makeWeekDayInfo";
-import handleRespObj from "./handleRespObj";
+import extractData from "./extractData";
 
 export default async function callAPIByLocation(locationName) {
   const fetchURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}/next7days?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslikemax%2Cfeelslike%2Chumidity%2Cprecipprob%2Cvisibility%2Cuvindex%2Csunrise%2Cmoonphase%2Cconditions%2Cdescription%2Cicon&key=24FXFJRPS3HUBDAAH3AFTTNJY&contentType=json`;
@@ -14,5 +14,5 @@ export default async function callAPIByLocation(locationName) {
   const day = new dayWeather(infoObject.days[0]);
   console.log(day);
 
-  handleRespObj(infoObject);
+  extractData(infoObject);
 }
--- src/modules/extractData.js
+++ src/modules/extractData.js
@@ -1,7 +1,7 @@
 import generalWeather from "./makeMainInfo";
 import dayWeather from "./makeWeekDayInfo";
 
-export default function handleRespObj(respObj) {
+export default function extractData(respObj) {
   const genWeather = new generalWeather(respObj);
   const weekArray = [];
   respObj.days.forEach((day) => {
