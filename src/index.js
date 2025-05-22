2c77e74a7a4670da5f35d1429810b64f7f5b6ef0
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-22 10:58:29 +0530

    Add dom structure for displaying weather.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import "./dom/dom.css";
 import addFooter from "./dom/footer";
 import renderLandingPage from "./dom/renderLandingPage";
 
-import callAPIByLocation from "./modules/callAPIByLocation";
-
-// callAPIByLocation("Kolhapur");
-
 document.addEventListener("DOMContentLoaded", () => {
   addFooter();
-  renderLandingPage();
+  // renderLandingPage();
 });
