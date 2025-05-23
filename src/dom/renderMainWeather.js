ab146df3dc0b15ba8cbb83f17ec71d3b46a2bb35
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-23 21:40:32 +0530

    Fix the resource import issue and also rename images.

--- src/dom/renderMainWeather.js
+++ src/dom/renderMainWeather.js
@@ -5,12 +5,42 @@ const genTempDiv = document.querySelector(".temp");
 const genTempMinMax = document.querySelector(".temp-minmax");
 const genDesc = document.querySelector(".weather-desc");
 const icon = document.querySelector(".weather-icon > img");
+const backDiv = document.querySelector(".day-weather");
 
-export default function renderMainWeather(generalWeather, dayInfo) {
+function dayOrNight(time) {
+  console.log(parseInt(time.slice(0, 3)));
+  if (parseInt(time.slice(0, 3)) >= 19) {
+    return "night";
+  } else {
+    return "day";
+  }
+}
+
+export default async function renderMainWeather(generalWeather, dayInfo) {
   locationDiv.innerText = generalWeather.resolvedAddress;
   timeDiv.innerText = generalWeather.localTime;
   dateDiv.innerText = dayInfo.date;
   genTempDiv.innerText = dayInfo.temp + "°c";
   genTempMinMax.innerText = dayInfo.tempMin + "°c / " + dayInfo.tempMax + "°c";
   genDesc.innerText = generalWeather.conditions;
+
+  const dayOrNightString = dayOrNight(generalWeather.localTime);
+  const weather = generalWeather.icon;
+  try {
+    // Dynamic import for SVG icon
+    const iconModule = await import(
+      `../resources/icons/${weather}-${dayOrNightString}.svg`
+    );
+    icon.src = iconModule.default;
+
+    // Dynamic import for background PNG
+    const bgModule = await import(
+      `../resources/backgrounds/${weather}-${dayOrNightString}.png`
+    );
+    backDiv.style.backgroundImage = `url(${bgModule.default})`;
+  } catch (err) {
+    console.error("Image loading error:", err);
+    icon.src = "";
+    backDiv.style.backgroundImage = "none";
+  }
 }
